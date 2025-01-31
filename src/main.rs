1633aef9f84183c0aa35314368b004f7ee54e85e
Talles Borges <talles.borges92@gmail.com>
2025-01-31 15:58:08 -0300

    feat: implment block types

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,12 @@ mod types {
 	pub type Balance = u128;
 	pub type BlockNumber = u32;
 	pub type Nonce = u32;
+	pub type Extrinsic = crate::support::Extrinsic<AccountId, crate::RuntimeCall>;
+	pub type Header = crate::support::Header<BlockNumber>;
+	pub type Block = crate::support::Block<Header, Extrinsic>;
 }
+
+pub enum RuntimeCall {}
 
 #[derive(Debug)]
 pub struct Runtime {
