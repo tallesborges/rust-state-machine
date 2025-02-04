43c05d83f7542c6b4f3810f4ab5004e89d174835
Talles Borges <talles.borges92@gmail.com>
2025-02-03 22:57:20 -0300

    feat: 6.3 add poe dispatch

--- src/proof_of_existence.rs
+++ src/proof_of_existence.rs
@@ -39,6 +39,23 @@ impl<T: Config> Pallet<T> {
 	}
 }
 
+pub enum Call<T: Config> {
+	CreateClaim { claim: T::Content },
+	RevokeClaim { claim: T::Content },
+}
+
+impl<T: Config> crate::support::Dispatch for Pallet<T> {
+	type Caller = T::AccountId;
+	type Call = Call<T>;
+
+	fn dispatch(&mut self, caller: Self::Caller, call: Self::Call) -> crate::support::DispatchResult {
+		match call {
+			Call::CreateClaim { claim } => self.create_claim(caller, claim),
+			Call::RevokeClaim { claim } => self.revoke_claim(caller, claim),
+		}
+	}
+}
+
 #[cfg(test)]
 mod test {
 	struct TestConfig;
